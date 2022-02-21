a43389ad7e4db10fcaf4e563e13864b6e016c0b7
wambuingare <wambuinancyngare@gmail.com>
2022-02-21 08:23:03 +0300

    i love coding

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "f2"
+
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main (){
+   greetings()
+    modulus()
+    sum()
+    funFact()
+
+
+}
+ fun greetings(){
+     var name ="Nancy"
+     println("Hello "+name)
+ }
+   fun modulus(){
+       var a =20
+       var b = 6
+       var modulus =a%b
+       println(modulus)
+   }
+    fun sum (){
+        var a = 10
+        var b = 5
+        var c = 15
+        var d = 40
+        var sum = a+b+c+d
+        println(sum)
+    }
+     fun funFact (){
+         var funFact = " i love singing"
+         println(funFact)
+     }
